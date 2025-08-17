b477663c3efac620778e98c60772209d9b539d8c
Ankit Singh <ankitsinghak9051@gmail.com>
2025-08-17 14:20:13 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,16 @@
 import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  projectId: 'sarkari-sahayak-y50gt',
-  appId: '1:83668272398:web:d9623abd2f8a1438f1835f',
-  storageBucket: 'sarkari-sahayak-y50gt.firebasestorage.app',
   apiKey: 'AIzaSyAvNcVi2rjHjWEpMiZY61uIwWbILGoP07M',
   authDomain: 'sarkari-sahayak-y50gt.firebaseapp.com',
-  measurementId: '',
+  projectId: 'sarkari-sahayak-y50gt',
+  storageBucket: 'sarkari-sahayak-y50gt.appspot.com',
   messagingSenderId: '83668272398',
+  appId: '1:83668272398:web:d9623abd2f8a1438f1835f',
+  measurementId: '', // optional
 };
 
-// Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 
