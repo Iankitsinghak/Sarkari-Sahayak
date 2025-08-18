de1deb5272570c5b23262e9fabb093446ee2b598
Ankit Singh <ankitsinghak9051@gmail.com>
2025-08-18 10:43:05 +0530

    Update use-auth.tsx

--- src/hooks/use-auth.tsx
+++ src/hooks/use-auth.tsx
@@ -1,12 +1,19 @@
-'use client';
+"use client";
 
-import { useContext } from 'react';
-import { AuthContext } from '@/components/auth-provider';
+import { useContext, useEffect } from "react";
+import { AuthContext } from "@/components/auth-provider";
+import { handleRedirectResult } from "@/services/auth-service";
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (context === undefined) {
-    throw new Error('useAuth must be used within an AuthProvider');
+    throw new Error("useAuth must be used within an AuthProvider");
   }
+
+  // On mount, complete redirect flow if it happened
+  useEffect(() => {
+    handleRedirectResult();
+  }, []);
+
   return context;
 };
